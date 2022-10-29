7c3d5ae9f9d103677032aeec856debf37ba50ba8
Eleriniara <eleriniara@gmail.com>
2022-10-29 09:43:07 -0400

    Функции ко 2 разделу

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function randomNumber(min, max) {
+  if (min < 0 || max < 0 ) {
+    return NaN;
+  }
+  let result = min + Math.random() * (max + 1 - min);
+  return Math.floor(result);
+}
+
+function stringLength(string, length) {
+  return string.length <= length;
+}
+randomNumber(1,7);
+stringLength('', 140);

87b4dfa5467aa7135d0f924531a0e7307a34fa90
Eleriniara <eleriniara@gmail.com>
2022-11-23 13:03:53 -0500

    ошибки

--- js/effect.js
+++ js/effect.js
@@ -1,9 +1,11 @@
 const image = document.querySelector('.img-upload__preview img');
-const effect = document.querySelector('input[type="radio"]');
+const effectList = document.querySelector('.effects__list');
 
-effect.onchange = function() {
-  const checked = document.querySelector('input[type="radio"]:checked');
-  if (checked)  {
-    image.classList.add('effects__preview');
+effectList.onchange = function(evt) {
+  if(evt.target.classList.contains('effects__radio')) {
+    const checked = document.querySelector('input[type="radio"]:checked');
+    if (checked) {
+      image.className = `effects__preview--${evt.target.id}`;
+    }
   }
 };

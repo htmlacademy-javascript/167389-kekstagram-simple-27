d43cb71d5d0ac576a333696fa00f7e1537d257a2
Keks <keks@htmlacademy.ru>
2022-11-23 17:20:36 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,11 @@
+import {allPhotoInfo, photoInfo} from './data.js';
+import './miniphoto.js';
+import './upload-form.js';
+import {openFormEdit, closeFormEdit} from './form.js';
+import './scale.js';
+
+photoInfo();
+allPhotoInfo();
+openFormEdit();
+closeFormEdit();
+
